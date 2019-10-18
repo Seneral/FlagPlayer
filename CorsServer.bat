347229cab9ef32bd3a655b24ad8af8f0597ae705
Seneral <12800840+Seneral@users.noreply.github.com>
2019-10-18 18:30:15 +0200

    Adapted server for public use - Confirmed runs on heroku - Added only official FlagPlayer host to whitelist (and local files when run wi> - Added strict rate limit of 50 request per 5 minutes

--- CorsServer.bat
+++
@@ -1,5 +0,0 @@
-ECHO OFF
-SET HOST=localhost
-SET PORT=8080
-node --max-http-header-size=65536 yt-server.js
-cmd /k
--- CorsServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")/"
-export HOST=localhost
-export PORT=8080
-node yt-server.js
